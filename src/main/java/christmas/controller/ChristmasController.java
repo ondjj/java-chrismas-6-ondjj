0f528f42b6ef9369b27da1ac3fab4630f528490d
Chang-Jun Jeon <jcjk0302@gmail.com>
2023-11-11 18:35:15 +0900

    feat(controller) : 컨트롤러 초석 구현

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -1,2 +1,27 @@
-package christmas.controller;public class ChrismasController {
+package christmas.controller;
+
+import christmas.dto.OrderItemDTO;
+import christmas.model.OrderItem;
+import christmas.model.VisitDate;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
+public class ChristmasController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public ChristmasController(final InputView inputView, final OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        inputView.welcome();
+        VisitDate visitDate = inputView.readDate();
+        OrderItem orderItem = inputView.readOrder();
+        OrderItemDTO orderItemDTO = orderItem.toDTO();
+        outputView.printFreeView();
+        outputView.printOrder(orderItemDTO);
+    }
+
 }
