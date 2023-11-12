6c7f89e3bf5075754e6531241a2684a37eaded5e
Chang-Jun Jeon <jcjk0302@gmail.com>
2023-11-13 01:24:50 +0900

    refactor(order) : order와 order-item 로직 수정

--- src/main/java/christmas/model/OrderItem.java
+++ src/main/java/christmas/model/OrderItem.java
@@ -1,10 +1,14 @@
 package christmas.model;
 
+import static christmas.util.Constants.ZERO;
 import static christmas.util.validators.OrderItemValidator.validFormat;
 import static christmas.util.validators.OrderItemValidator.validateOrderData;
 
 import christmas.dto.OrderItemDTO;
 import christmas.util.Parser;
+import christmas.util.enums.Menu;
+import christmas.util.enums.MenuType;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class OrderItem {
@@ -12,11 +16,12 @@ public class OrderItem {
 
     private OrderItem(Map<String,Integer> data) {
         this.item = data;
+        validateDrinkOrder();
     }
 
     public static OrderItem of(String date) {
         validFormat(date);
-        Map<String,Integer> parseData = Parser.parseString(date);
+        Map<String, Integer> parseData = Parser.parseString(date);
         validateOrderData(parseData);
         return new OrderItem(parseData);
     }
@@ -26,6 +31,30 @@ public class OrderItem {
     }
 
     public int getCount(String itemName) {
-        return item.getOrDefault(itemName, 0);
+        return item.getOrDefault(itemName, ZERO);
+    }
+
+    public Map<MenuType, Integer> getItemQuantity() {
+        Map<MenuType, Integer> menuCount = new LinkedHashMap<>();
+
+        for (Map.Entry<String, Integer> entry : item.entrySet()) {
+            String itemName = entry.getKey();
+            Integer quantity = entry.getValue();
+            Menu menu = Menu.getMenuByName(itemName);
+            menuCount.put(menu.getType(), menuCount.getOrDefault(menu.getType(), ZERO) + quantity);
+        }
+        return menuCount;
+    }
+
+    private void validateDrinkOrder() {
+        if (item.keySet().stream().allMatch(menuName -> Menu.getMenuByName(menuName).getType() == MenuType.DRINK)) {
+            throw new IllegalArgumentException("Drink menu cannot be ordered alone.");
+        }
+    }
+
+    public Integer getTotalPrice() {
+        return item.entrySet().stream()
+                .mapToInt(entry -> Menu.getMenuByName(entry.getKey()).getPrice() * entry.getValue())
+                .sum();
     }
 }
