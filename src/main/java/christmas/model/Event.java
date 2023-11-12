5936929450be3b519f686609b397ad9767d872a6
Chang-Jun Jeon <jcjk0302@gmail.com>
2023-11-13 01:25:20 +0900

    refactor(event) : interface 수정

--- src/main/java/christmas/model/Event.java
+++ src/main/java/christmas/model/Event.java
@@ -1,6 +1,11 @@
 package christmas.model;
 
 
+import christmas.util.enums.EventType;
+import java.util.Map;
+
 public interface Event {
     Integer itemDiscount();
+    EventType getEventType();
+    Map<String, String> extractEventDetails();
 }
