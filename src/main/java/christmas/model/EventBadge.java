4187ddee1ae05b974832124f271a20a59bd06011
Chang-Jun Jeon <jcjk0302@gmail.com>
2023-11-13 01:25:43 +0900

    feat(badge) : 기능 구현

--- src/main/java/christmas/model/EventBadge.java
+++ src/main/java/christmas/model/EventBadge.java
@@ -1,2 +1,27 @@
-package christmas.model;public class EventBadge {
+package christmas.model;
+
+import christmas.util.enums.Badge;
+
+public class EventBadge {
+    private final Integer totalPrice;
+
+    private EventBadge(Integer totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public static EventBadge of(Integer totalPrice) {
+        return new EventBadge(totalPrice);
+    }
+
+    public String determineBadge() {
+        if (!isValidEvent()) {
+            return "없음";
+        }
+        Badge badge = Badge.getBadgeByAmount(this.totalPrice);
+        return badge.getBadgeName();
+    }
+
+    private boolean isValidEvent() {
+        return this.totalPrice >= 10000;
+    }
 }
--- src/main/java/christmas/util/enums/Badge.java
+++ src/main/java/christmas/util/enums/Badge.java
@@ -1,2 +1,33 @@
-package christmas.util.enums;public enum Badge {
+package christmas.util.enums;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public enum Badge {
+    STAR("별", 5000),
+    TREE("트리", 10000),
+    SANTA("산타", 20000);
+
+    private final String badgeName;
+    private final Integer badgeAmount;
+
+    Badge(String badgeName, Integer badgeAmount) {
+        this.badgeName = badgeName;
+        this.badgeAmount = badgeAmount;
+    }
+
+    public String getBadgeName() {
+        return badgeName;
+    }
+
+    public Integer getBadgeAmount() {
+        return badgeAmount;
+    }
+
+    public static Badge getBadgeByAmount(Integer amount) {
+        return Arrays.stream(Badge.values())
+                .filter(badge -> amount >= badge.getBadgeAmount())
+                .max(Comparator.comparing(Badge::getBadgeAmount))
+                .orElse(null);
+    }
 }
